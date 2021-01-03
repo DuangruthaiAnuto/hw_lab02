b60a56dbb10c9d3f2be88005fab70d109861e03b
DUANGRUTHAI\Duangruthai <614234005@parichat.skru.ac.th>
2021-01-03 13:54:48 +0700

    Super Friend

--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,123 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(SuperFriend());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class SuperFriend extends StatelessWidget {
+  final List<String> _friendImages = [
+    "images/lip.jpg",
+    "images/arm.jpg",
+    "images/baw.jpg",
+    "images/nueng.jpg",
+  ];
+  final List<String> _friendNames = [
+    "ผู้ชายตัวเล็ก",
+    "แม่ค้าปลากัด",
+    "ความหวังหมู่บ้าน",
+    "ผู้ชายกลัวน้ำ",
+  ];
+  final List<String> _friendDetails = [
+    "ลิป : เป็นเพื่อนที่พูดน้อย ไม่ค่อยกล้าแสดงออก แต่อัธยาศัยดี มีอะไรให้ช่วยแค่ขอให้บอก",
+    "อาร์ม : เป็นเพื่อนที่พูดมาก และถ้าคิดจะขาดเรียน อาร์มจะโทรจิกจนต้องมาเรียน",
+    "บ่าว : เป็นเพื่อนที่พูดตรงไปตรงมา ขอความช่วยเหลืออะไรไป บ่าวช่วยตลอด",
+    "หนึ่ง : เป็นเพื่อนที่ไม่ค่อยชอบอาบน้ำ แต่ใจดี คิดบวก เป็นที่ระบายได้หลาย ๆ เรื่อง",
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+      home: Scaffold(
+        backgroundColor: Colors.grey[900],
+        appBar: AppBar(
+          backgroundColor: Colors.pinkAccent[700],
+          title: Text("Super Friend"),
+        ),
+        body: SafeArea(
+          child: ListView(
+            padding: EdgeInsets.all(10),
+            children: [
+              Column(
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    margin: EdgeInsets.all(20),
+                    width: double.infinity,
+                    color: Colors.pinkAccent[700],
+                    child: Center(
+                      child: Text(
+                        "Super Friend",
+                        style: TextStyle(
+                            fontSize: 30, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                  Friend(this._friendNames[0], this._friendImages[0],
+                      this._friendDetails[0]),
+                  Friend(this._friendNames[1], this._friendImages[1],
+                      this._friendDetails[1]),
+                  Friend(this._friendNames[2], this._friendImages[2],
+                      this._friendDetails[2]),
+                  Friend(this._friendNames[3], this._friendImages[3],
+                      this._friendDetails[3]),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class Friend extends StatelessWidget {
+  String _friendNames;
+  String _friendImages;
+  String _friendDetails;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  Friend(this._friendNames, this._friendImages, this._friendDetails, {Key key})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+    return Card(
+      child: Container(
+        color: Colors.pinkAccent[100],
+        width: double.infinity,
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+          children: [
+            SizedBox(
+              height: 10,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            CircleAvatar(
+              radius: 50,
+              backgroundImage: AssetImage(this._friendImages),
+            ),
+            SizedBox(height: 10),
+            Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.favorite, color: Colors.redAccent[400]),
+                  Text(
+                    this._friendNames,
+                    style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.pink[900],
+                        fontWeight: FontWeight.bold),
+                  ),
+                  Icon(Icons.favorite, color: Colors.redAccent[400])
+                ],
+              ),
+            ),
+            Container(
+              child: Text(this._friendDetails),
+              padding: EdgeInsets.all(25),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
